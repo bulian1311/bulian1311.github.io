3b776cd8778a069978d91a6219a291982994f484
Nikolay <bulian1311@gmail.com>
2020-07-21 14:55:26 +0300

    build 02

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.05389e6cb573f8fb2ce129984c425549.js"
+  "/precache-manifest.3b49afaf101fa18a600e5e980039a275.js"
 );
 
 self.addEventListener('message', (event) => {

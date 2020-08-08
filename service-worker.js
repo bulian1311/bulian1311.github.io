d749782f979bcbb7f5f51d4542d189cf92d18938
Kachanov2508 <kachanov2508@yandex.ru>
2020-08-08 20:58:32 +0300

    form adaptiv

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a13437217a41827e00057a97a6cbaab.js"
+  "/precache-manifest.9adbbac4701d7c2bf9e5da6d9396aa68.js"
 );
 
 self.addEventListener('message', (event) => {
